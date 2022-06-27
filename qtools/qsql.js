afe8288c4e47e158e08b558658b5991cca3577ea
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-27 14:13:32 +0200

    data path bug fixed..

--- qtools/qsql.js
+++ qtools/qsql.js
@@ -1,16 +1,15 @@
-import sqlite3 from 'sqlite3';
-import * as qsys from './qsys.js';
+import sqlite3 from "sqlite3";
+import * as qsys from "./qsys.js";
 
 //view and manage SQLite files on Windows/Mac/Linux with: https://sqlitebrowser.org/dl
-const absoluteDbPathAndFileName = qsys.buildSystemAbsolutePathAndFileName(
-	'src/data/site.sqlite'
-);
+const absoluteDbPathAndFileName =
+  qsys.buildSystemAbsolutePathAndFileName("data/site.sqlite");
 
 /**
  * Returns promise for an array of objects based on an SQL query.
- * 
+ *
  * return await qsql.getRecordsWithSql('SELECT * FROM colors');
- * 
+ *
  * [
  *   {
  *      id: 1,
@@ -23,17 +22,17 @@ const absoluteDbPathAndFileName = qsys.buildSystemAbsolutePathAndFileName(
  * ]
  */
 export const getRecordsWithSql = (sql) => {
-	return new Promise((resolve, reject) => {
-		const db = new sqlite3.Database(absoluteDbPathAndFileName);
-		db.all(sql, function (err, records) {
-			if (records === undefined) {
-				reject(err);
-			} else if (records.length === 0) {
-				resolve([]);
-			} else {
-				resolve(records);
-			}
-		});
-		db.close();
-	});
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(absoluteDbPathAndFileName);
+    db.all(sql, function (err, records) {
+      if (records === undefined) {
+        reject(err);
+      } else if (records.length === 0) {
+        resolve([]);
+      } else {
+        resolve(records);
+      }
+    });
+    db.close();
+  });
 };
